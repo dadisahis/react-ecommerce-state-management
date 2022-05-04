0f95fd9eefc425ae8910e75550e03d4cbd27761c
Adithya Saish <adiwithmj@gmail.com>
2022-05-04 16:57:56 +0530

    added routes to navigate between pages using hooks like useNavigate,useParams from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,25 @@
-import React,{useState,} from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Spinner from './Spinner'
-import useFetch from "./services/useFetch";
+import Products from "./Products";
+import { Route,Routes } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
 
 export default function App() {
-  const [size, setSize] = useState("");
-
-  const {data: products, loading,error} = useFetch("products?category=shoes");
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-          <p>{p.skus.map(obj => obj.size + ' ')}</p>
-        </a>
-      </div>
-    );
-  }
-  const filteredProducts = size ? products.filter(p => p.skus.find(s => s.size === Number.parseInt(size))) : products;
-  if (error){
-    throw error;
-  }
-  if (loading){
-    return <Spinner />
-  }
   return (
     <>
       <div className="content">
         <Header />
-        <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            {size && <h2>Found {filteredProducts.length} items</h2>}
-          </section>
-          <section id="products">{filteredProducts.map(product => renderProduct(product))}</section>
-        </main>
+          <main>
+            <Routes>
+              <Route path="/" element={<h1>Welcome to Carved Rock Fitness </h1>} />
+              <Route path="/:category" element={<Products />} />
+              <Route path="/:category/:id" element={<Detail />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </main>
       </div>
       <Footer />
     </>
