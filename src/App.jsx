57b9ebb8e4070efb710cefb6235ef3ca6d3e54e3
Adithya Saish <adiwithmj@gmail.com>
2022-05-06 11:45:43 +0530

    added checkout logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./Products";
 import { Route,Routes } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 export default function App() {
   const [cart,setCart] = useState(
@@ -40,6 +41,9 @@ export default function App() {
       items.map((i) => i.sku ===sku ? {...i, quantity} : i)
     })
   }
+  function emptyCart(){
+    setCart([]);
+  }
   return (
     <>
       <div className="content">
@@ -50,6 +54,7 @@ export default function App() {
               <Route path="/:category" element={<Products />} />
               <Route path="/:category/:id" element={<Detail addtoCart={addtoCart} />} />
               <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity} />} />
+              <Route path="/checkout" element={<Checkout cart={cart} emptyCart={emptyCart} />} />
             </Routes>
           </main>
       </div>
